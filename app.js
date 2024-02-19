42dcd31b1a076e10a0fdd6e06604e6ef6977333d
lucas-henrique-lucas <159969857+lucas-henrique-lucas@users.noreply.github.com>
2024-02-19 02:19:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+function adicionarTarefa() {
+    let texto = prompt('Anote:');
+
+    let item_lista = document.createElement('li');
+    manipularAtributo(item_lista, 'class', 'lista__item');
+
+    let botao_lista_remover = document.createElement('button');
+    manipularAtributo(botao_lista_remover, 'class', 'remover');
+    manipularAtributo(botao_lista_remover, 'onclick', 'removerTarefa(this)');
+    exibirIconeDoBotao('./img/trash-2.svg', botao_lista_remover);
+
+    let botao_lista_concluir = document.createElement('button');
+    manipularAtributo(botao_lista_concluir, 'class', 'concluir');
+    manipularAtributo(botao_lista_concluir, 'onclick', 'concluirTarefa(this)');
+    exibirIconeDoBotao('./img/check.svg', botao_lista_concluir);
+
+    let texto_lista = document.createElement('p');
+    manipularAtributo(texto_lista, 'class', 'lista__item__texto');
+
+    texto_lista.textContent = texto;
+
+    document.getElementById('lista-pendencia').appendChild(item_lista).appendChild(botao_lista_remover);
+    document.getElementById('lista-pendencia').appendChild(item_lista).appendChild(botao_lista_concluir);
+    document.getElementById('lista-pendencia').appendChild(item_lista).appendChild(texto_lista);
+}
+
+function limparCampo() {
+    const pendencia = document.getElementById('lista-pendencia');
+    const  concluido = document.getElementById('lista-concluido');
+
+    while (pendencia.hasChildNodes()) {
+      pendencia.removeChild(pendencia.firstChild);
+    }
+
+    while (concluido.hasChildNodes) {
+        concluido.removeChild(concluido.firstChild);
+    }
+}
+
+function exibirIconeDoBotao(caminho, botao) {
+    let img = document.createElement('img');
+    manipularAtributo(img, 'src', caminho)
+    botao.appendChild(img);
+}
+
+function manipularAtributo(elemento, atributo, nome) {
+    elemento.setAttribute(atributo, nome);
+}
+
+function removerTarefa(este) {
+    let elemento = este.parentNode;
+    elemento.remove()
+}
+
+function concluirTarefa(este) {
+    let elemento = este.parentNode;
+    let img = este.firstChild;
+    if (elemento.parentNode.id == 'lista-pendencia') {
+        img.src = './img/x.svg';
+        document.getElementById('lista-concluido').appendChild(elemento);
+    } else {
+        img.src = './img/check.svg';
+        document.getElementById('lista-pendencia').appendChild(elemento);
+    }
+}
